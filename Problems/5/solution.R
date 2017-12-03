f29acc04b298bf686da0a8b3c24717cb1c352985
Sean <sme917@mail.usask.ca>
2017-12-03 01:59:00 -0500

    added solution for 5

--- Problems/5/solution.R
+++ Problems/5/solution.R
@@ -4,3 +4,27 @@
 
 rm(list=ls())
 cat('\014')
+
+all.divisible.flag=F
+check.integers = c(3,6,7,8,9,11,12,13,14,15,16,17,18,19,20)
+
+h=0
+while(all.divisible.flag==F){
+  h=h+1
+  i=h*20
+  
+ 
+    print(i)
+  
+  
+  divisable=c()
+  k=0
+  for(j in check.integers){
+    k=k+1
+    divisable[k]=i %% j
+  }
+  
+  if(all(divisable==0)){
+    all.divisible.flag=T
+  }
+}
