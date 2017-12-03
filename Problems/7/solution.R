63d59ad407a94c22791c2f604ddc52582f3dbe4e
Sean <sme917@mail.usask.ca>
2017-12-03 03:01:57 -0500

    added solutions to 7 and 8

---
+++ Problems/7/solution.R
@@ -0,0 +1,28 @@
+#PE Problem 7
+#What is the 10 001st prime number?
+
+rm(list=ls())
+cat('\014')
+
+j=1
+i=1
+start=Sys.time()
+while(j<10001){
+  i=i+2
+  
+  check.list = seq(2,ceiling(sqrt(i)))
+  
+  factor.check=c()
+  for(k in check.list){
+    factor.check = c(factor.check,i %% k)
+  }
+  
+  if(all(factor.check!=0)==T){
+    j=j+1
+    print(j)
+  }
+}
+end=Sys.time()
+print(start)
+print(end)
+print(i)
